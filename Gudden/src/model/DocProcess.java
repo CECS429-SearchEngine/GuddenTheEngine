db40445e3f2da42d1146eae2fef18a4a5b76f181
Crystal Chun <chun_crystal7@yahoo.com>
2017-09-18 17:01:58 -0700

    Added functionality for processing a document rather than just one string.

--- Gudden/src/model/DocProcess.java
+++ Gudden/src/model/DocProcess.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //import java.util.regex.Pattern;
 
 /**
@@ -15,10 +18,34 @@ public class DocProcess
 		
 	}
 	
-	public void process(String word) // Replace with Document object 
+	public List<String> process(String word /*Document doc*/) // Replace with Document object 
 	{
 		System.out.println("Word: " + word);
 		
+		ArrayList <String> terms = new ArrayList<String> ();
+		// Uncomment when have document element
+		/*for(String term: doc.terms()) // Change this method
+		{
+			// Removes apostrophes, non-alphanumeric characters at beginning and end, and converts to lowercase
+			term = term.replaceAll("^\\W*", "");
+			term = term.replaceAll("\\W*$", "");
+			term = term.replaceAll("\'", "");
+			term = term.toLower();
+			
+			// Splits term on hyphen and removes hyphen
+			if(term.contains("-"))
+			{
+				String [] words = word.split("-");
+				String wordNoHyphen = word.replaceAll("-", "");
+				terms.add(wordNoHyphen);
+				// Return the array and var and this word 
+				for(String word: words)
+				{
+					terms.add(word);
+				}	
+			}
+			terms.add(term);
+		}*/
 		// Remove non-alphanumeric chars from begin and end
 		System.out.println("Replacing non-alphanumeric characters in beginning of word . . .");
 		word = word.replaceAll("^\\W*", "");
@@ -39,5 +66,7 @@ public class DocProcess
 			// Return the array and var and this word 
 			
 		}
+		
+		return terms;
 	}
 }
