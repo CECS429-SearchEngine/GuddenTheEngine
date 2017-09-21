f4da392729b9ef15f1a19cef0c38ddd547d73d05
Crystal Chun <chun_crystal7@yahoo.com>
2017-09-21 11:19:59 -0700

    Updated DocProcess to process words from a document.

--- Gudden/src/model/DocProcess.java
+++ Gudden/src/model/DocProcess.java
@@ -3,8 +3,6 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-//import java.util.regex.Pattern;
-
 /**
  * Class for processing documents to build tokens
  * @author crystalchun
@@ -18,25 +16,24 @@ public class DocProcess
 		
 	}
 	
-	public List<String> process(String word /*Document doc*/) // Replace with Document object 
-	{
-		System.out.println("Word: " + word);
-		
+	public List<String> process(Document doc) // Replace with Document object 
+	{		
 		ArrayList <String> terms = new ArrayList<String> ();
-		// Uncomment when have document element
-		/*for(String term: doc.terms()) // Change this method
+
+		String [] vocab = doc.getBody().split(" ");
+		for(String term: vocab)
 		{
 			// Removes apostrophes, non-alphanumeric characters at beginning and end, and converts to lowercase
 			term = term.replaceAll("^\\W*", "");
 			term = term.replaceAll("\\W*$", "");
 			term = term.replaceAll("\'", "");
-			term = term.toLower();
+			term = term.toLowerCase();
 			
 			// Splits term on hyphen and removes hyphen
 			if(term.contains("-"))
 			{
-				String [] words = word.split("-");
-				String wordNoHyphen = word.replaceAll("-", "");
+				String [] words = term.split("-");
+				String wordNoHyphen = term.replaceAll("-", "");
 				terms.add(wordNoHyphen);
 				// Return the array and var and this word 
 				for(String word: words)
@@ -45,28 +42,7 @@ public class DocProcess
 				}	
 			}
 			terms.add(term);
-		}*/
-		// Remove non-alphanumeric chars from begin and end
-		System.out.println("Replacing non-alphanumeric characters in beginning of word . . .");
-		word = word.replaceAll("^\\W*", "");
-		System.out.println("Replaced non-alphanumeric characters in beginning: " + word);
-		System.out.println("Replacing non-alphanumeric characters at end of word . . .");
-		word = word.replaceAll("\\W*$", "");
-		System.out.println("Replaced non-alphanumeric characters in end: " + word);
-		
-		// Remove all apostrophes
-		System.out.println("Removing apostrophes");
-		word = word.replaceAll("\'", "");
-		System.out.println("Final word: " + word);
-		
-		if(word.contains("-"))
-		{
-			String [] words = word.split("-");
-			String wordNoHyphen = word.replaceAll("-", "");
-			// Return the array and var and this word 
-			
 		}
-		
 		return terms;
 	}
 }
