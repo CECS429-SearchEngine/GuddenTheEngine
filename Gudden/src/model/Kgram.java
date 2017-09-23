15b4baab229ad89131241578bf2d82dfff8215e6
Crystal Chun <chun_crystal7@yahoo.com>
2017-09-23 16:23:52 -0700

    Updated K-Gram index class to match style and added a get method for the list a gram is matched to.

--- Gudden/src/model/Kgram.java
+++ Gudden/src/model/Kgram.java
@@ -7,27 +7,22 @@ import java.util.List;
 public class Kgram {
 
 	/**All of the k-grams and their mappings to words that match it*/
-	private HashMap<String, List<String>> k_gram;
+	private HashMap <String, List<String>> k_gram;
 	
-	public Kgram()
-	{
+	public Kgram() {
 		k_gram = new HashMap<String, List<String>> ();
 	}
 	
-	public void add(String term)
-	{
-		// First prepare the term to be a k_gram index
+	public void add(String term) {
 		// Add the dollar signs
 		term = "$" + term + "$";
 		
-		for(int i = 1; i <= 3; i++)
-		{
+		for(int i = 1; i <= 3; i++) {
 			split(i,term);
 		}
 	}
 	
-	public HashMap<String, List<String>> getKGram()
-	{
+	public HashMap<String, List<String>> getKGram() {
 		return k_gram;
 	}
 	
@@ -36,15 +31,14 @@ public class Kgram {
 	 * @param k The size of the gram
 	 * @param term The term
 	 */
-	private void split(int k, String term)
-	{
+	private void split(int k, String term) {
 		int i = 0;
 
 		// Split into grams and add into list then return list
-		while(i + k < term.length())
-		{
+		while(i + k < term.length()) {
 			String key = "";
 			key = term.substring(i, i + k);
+			
 			addKey(key, term);
 			i++;
 		}
@@ -55,23 +49,25 @@ public class Kgram {
 	 * @param key The key for this term.
 	 * @param term The term this key maps to.
 	 */
-	private void addKey(String key, String term)
-	{
+	private void addKey(String key, String term) {
+		
 		// Then check current k_gram hashmap to see if gram already exists
-		if(k_gram.containsKey(key))
-		{
+		if(k_gram.containsKey(key)) {
 			// Check if k_gram's list contains term
-			if(!(k_gram.get(key)).contains(term))
-			{
+			if(!(k_gram.get(key)).contains(term)) {
 				(k_gram.get(key)).add(term);
 			}
 		}
-		else
-		{
+		else {
 			// Create a new list for k_gram and insert term into it
 			ArrayList <String> list = new ArrayList <String> ();
+			
 			list.add(term);
 			k_gram.put(key, list);
 		}
+	}
+	
+	public List<String> getTerms(String key) {
+		return k_gram.get(key);
 	}
 }
